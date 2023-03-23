6a5099db54eeb188beaadf54f07111092cb882bf
TheUnknovvn <baonpg.bi12-048@st.usth.edu.vn>
2023-03-24 03:54:38 +0700

    "Update class work 2, might need to rewrite student and course to be polars Dataframe too"

--- Testing/test_code.py
+++ Testing/test_code.py
@@ -1,10 +1,15 @@
 # ____________________LIBRARIES AND CONFIGS____________________
 
 from dataclasses import dataclass
-import os
-os.system('pip install polars')
-import polars as pl
+import math
+import numpy as np
+try:
+    import polars as pl
+except ModuleNotFoundError:
+    import os
+    os.system('pip install polars')
 pl.Config.set_tbl_hide_column_data_types(True)
+pl.Config.set_tbl_hide_dataframe_shape(True)
 
 # ____________________DATA SECTION____________________
 
@@ -12,8 +17,6 @@ students = []
 students_name = []
 courses = []
 courses_name = []
-marked_courses = []
-marked_courses_name = []
 num_student: int
 num_courses: int
 
@@ -63,7 +66,7 @@ class Student(Base):
 
     @classmethod
     def get_input(cls):
-        print("\nEnter the data for student.\n")
+        print("Enter the data for student.\n")
         x = super().get_input()
         input_dob = input("Enter the date of birth: ")
         return Student(x.name.title(), x.id, input_dob)
@@ -78,14 +81,35 @@ class Student(Base):
         Date of birth: {self.dob}"""
 
 
-@dataclass
+# @dataclass
 class Course(Base):
+
+    __credit: int
+
+    @property
+    def credit(self):
+        return self.__credit
+    
+    @credit.setter
+    def credit(self, input_credit):
+        self.credit = input_credit
+
+    def __init__(self, input_name, input_id, input_credit):
+        self.id = input_id
+        self.name = input_name
+        self.dob = input_credit
 
     @classmethod
     def get_input(cls):
-        print("\nEnter the data for course.\n")
+        print("Enter the data for course.\n")
         x = super().get_input()
-        return Course(x.name.capitalize(), x.id)
+        while True:
+            input_credit = input("Enter the number of credit for this course: ")
+            if validate_number(input_credit):
+                break
+            else:
+                print(f"\"{input_credit}\" is not a valid value. Must be an integer and at least 1.\n")
+        return Course(x.name.capitalize(), x.id, input_credit)
 
     def __repr__(self) -> str:
         return f"""Course {self.name}:
@@ -93,7 +117,20 @@ class Course(Base):
 
 
 class Mark(pl.DataFrame):
-    pass
+    def add_subject(self, subject_name, mark_list):
+        return self.with_columns(
+            [
+                pl.Series(subject_name, mark_list)
+            ]
+        )
+
+    def add_student(self, student_list):
+        return self.select(
+            [
+                pl.Series("Name", student_list),
+                pl.all()
+            ]
+        )
 
 
 # ____________________FUNCTION SECTION____________________
@@ -110,6 +147,7 @@ def validate_mark(x: str):
         return (float(str(x)) >= 0) and (float(str(x)) <= 10)
     except ValueError:
         return False
+
 
 def get_num(input_type: str) -> str:
     while True:
@@ -140,10 +178,10 @@ def enter_course(num_course, courses, courses_name):
         print()
 
 
-def enter_mark():
+def enter_mark(c: Mark):
     # Enter course
     while True:
-        mark_course = input("Enter the course to give mark: ")
+        mark_course = input("Enter the course to give mark: ").capitalize()
         if (mark_course in courses_name):
             break
         print(f"{mark_course} is not an available course\n")
@@ -153,15 +191,44 @@ def enter_mark():
         while True:
             student_grade = input(f"Enter the mark for {x}: ")
             if validate_mark(student_grade):
-                mark_list.append(student_grade)
+                mark_list.append(round(student_grade, 1))
                 break
             else:
-                print(f"\"{student_grade}\" is not a valid value. Must be a number between 0 and 10.\n")
-    marked_courses.append(mark_course)
-    return({mark_course:mark_list})
+                print(
+                    f"\"{student_grade}\" is not a valid value. Must be a number between 0 and 10.\n")
+    return c.add_subject(mark_course, mark_list)
+
+
+def list_students(students):
+    if (len(students) == 0):
+        print("There is no data for students to list yet.\n")
+    else:
+        print(f"\nThere are {len(students)} students")
+        for student in students:
+            print(student)
+
+
+def list_courses(courses):
+    if (len(courses) == 0):
+        print("\nThere is no data for courses to list yet.\n")
+    else:
+        print(f"There are {len(courses)} courses")
+        for course in courses:
+            print(course)
+
+
+def list_mark(c: Mark):
+    while True:
+        mark_course = input("Enter the course to give mark: ").capitalize()
+        if (mark_course in c.columns):
+            break
+        print(
+            f"{mark_course} is not an available course or not a course that has mark\n")
+    print(c.select(["Name", mark_course]))
 
 
 def main():
+    marks = Mark()
     while True:
         choice = input(
             "What do you want to do. Input data(I/i), List data(L/l) or exit(E/e): ").lower()
@@ -172,6 +239,7 @@ def main():
                 if choice == "s":
                     num_student = get_num("student")
                     enter_students(num_student, students, students_name)
+                    marks = marks.add_student(students_name)
                     break
                 elif choice == "c":
                     num_course = get_num("course")
@@ -179,7 +247,7 @@ def main():
                     break
                 elif choice == "m":
                     if (len(students_name)*len(courses_name) != 0):
-                        enter_mark(mark_c_name, mark_courses)
+                        marks = enter_mark(marks)
                     else:
                         print(
                             "There is no data for either student or course, can't enter mark now.\n")
@@ -190,36 +258,34 @@ def main():
         elif choice == "l":
             while True:
                 choice = input(
-                    "Which do you want to list, student(S/s), course(C/c) or mark(M/m) data: ").lower()
+                    "\nWhich do you want to list, student(S/s), course(C/c), mark(M/m) or GPA(G/g) data: ").lower()
                 if choice == "s":
-                    list_student(students)
+                    list_students(students)
                     break
                 elif choice == "c":
-                    list_course(courses)
+                    list_courses(courses)
                     break
                 elif choice == "m":
-                    list_mark(mark_courses, mark_c_name)
+                    if (len(students_name)*len(courses_name) != 0) and (marks.shape[1] > 1):
+                        list_mark(marks)
+                    else:
+                        print(
+                            "There is no data for either student or course or there is no data for mark yet, can't list marks now.\n")
                     break
+                elif choice == "g":
+                    pass
                 else:
                     print(
-                        f"Option {choice} is not available, please try again.\n")
+                        f"Option {choice} is not available, please try again.")
                 pass
         elif choice == "e":
             print("Thanks for using. Goodbye!\n")
             exit()
         else:
-            print(f"Option {choice} is not available, please try again.\n")
+            print(f"Option \"{choice}\" is not available, please try again.\n")
 
 
 # ___________________MAIN SECTION___________________
 
 if __name__ == "__main__":
     main()
-
-
-# ___________________TESTING___________________
-# a = Course.get_input()
-# b = Student.get_input()
-# print(a)
-# print(b)
-c = Mark({"Math": [1, 2, 3]})
